bdcbd06154341d3f4d6e55209c350ff0cbfea4ef
David Adams <davebenaaa@gmail.com>
2025-11-07 13:53:53 +0100

    next-config-file-update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-// next.config.js
-const nextConfig = {
-  // ✅ explicitly tell Next.js to use Webpack instead of Turbopack
+import type { NextConfig } from "next";
 
-  webpack: (config: { resolve: { alias: { [x: string]: string } } }) => {
-    // ✅ Force react-hook-form to use client build
-    config.resolve.alias["react-hook-form"] = require.resolve(
-      "react-hook-form/dist/index.mjs"
-    );
-    return config;
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
